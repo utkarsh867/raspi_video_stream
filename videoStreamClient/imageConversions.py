a4c104bd84481fec0cc824590281e341b730047c
utkarsh867 <utkarsh867@gmail.com>
2018-02-14 01:30:11 +0800

    fixed exceptions

--- videoStreamClient/imageConversions.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import cv2
-
-
-def nd_arr(byte_string):
-    nparr = np.fromstring(byte_string, np.uint8)
-    img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    return img_np
--- videoStreamClient/videoStreamClient.py
+++ videoStreamClient/videoStreamClient.py
@@ -1,37 +1,34 @@
-from flask import Flask, Response, render_template
 import requests
-import imageConversions as ic
+import numpy as np
 import cv2
 
-app = Flask(__name__)
+
+def nd_arr(byte_string):
+    nparr = np.fromstring(byte_string, np.uint8)
+    img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    return img_np
 
 
 def get_stream():
     while True:
-        frame_bytes = requests.get('http://localhost:5000/stream', stream=True)
-        if frame_bytes.status_code == 200:
-            data = bytes()
-            for chunk in frame_bytes.iter_content(chunk_size=1024):
-                data += chunk
-                a = data.find(b'\xff\xd8')
-                b = data.find(b'\xff\xd9')
-                if a != -1 and b != -1:
-                    final_frame = data[a:b + 2]
-                    data = data[b + 2:]
-                    frame = ic.nd_arr(final_frame)
-                    cv2.imshow('Video feed', frame)
-                    if cv2.waitKey(1) & 0xFF == ord('q'):
-                        exit(0)
-
-
-@app.route('/')
-def index():
-    main()
-
-
-@app.route('/stream')
-def get_video():
-    return Response(get_stream(), mimetype='multipart/x-mixed-replace;boundary=frame')
+        try:
+            # If the connection is established
+            frame_bytes = requests.get('http://147.8.233.230:5000/stream', stream=True)
+            if frame_bytes.status_code == 200:
+                data = bytes()
+                for chunk in frame_bytes.iter_content(chunk_size=1024):
+                    data += chunk
+                    a = data.find(b'\xff\xd8')
+                    b = data.find(b'\xff\xd9')
+                    if a != -1 and b != -1:
+                        final_frame = data[a:b + 2]
+                        data = data[b + 2:]
+                        frame = nd_arr(final_frame)
+                        cv2.imshow('Video feed', frame)
+                        if cv2.waitKey(1) & 0xFF == ord('q'):
+                            exit(0)
+        except requests.exceptions.ConnectionError:
+            print("Could not connect")
 
 
 def main():
